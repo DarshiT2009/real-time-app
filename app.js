c700a548cb9bd3c1dfb27529bde539eb6affedb1
Darshit sodhatra <d55649694@gmail.com>
2024-12-10 16:04:17 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const http = require("http");
+const socketio = require("socket.io");
+
+const server = http.createServer(app);
+const io = socketio(server);
+
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public"))); 
+
+io.on("connection", function (socket) {
+  console.log(`User connected: ${socket.id}`);
+  
+  socket.on("send-location", function (data) {
+    io.emit("receive-location", { id: socket.id, ...data });
+  });
+  
+  socket.on("disconnect", function () {
+    console.log(`User disconnected: ${socket.id}`);
+    io.emit("user-disconnected", socket.id);
+  });
+});
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+server.listen(3001, () => {
+  console.log("Server running on http://localhost:3001");
+});
